1d6164a054aae9a0f8cf1307f7ceb8ee7d0bd6d8
t_funato <t_funato@learnie-inc.com>
2018-11-16 22:32:39 +0900

    first commit

---
+++ src/main/java/jp/canetrash/kinesis/sample/SampleApplication.java
@@ -0,0 +1,12 @@
+package jp.canetrash.kinesis.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+}
