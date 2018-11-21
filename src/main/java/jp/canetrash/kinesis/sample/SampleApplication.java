61d21d0fd73f58b3d58f132dad1a5b26efd7477a
t_funato <t_funato@learnie-inc.com>
2018-11-22 01:29:16 +0900

    add dependency

--- src/main/java/jp/canetrash/kinesis/sample/SampleApplication.java
+++ src/main/java/jp/canetrash/kinesis/sample/SampleApplication.java
@@ -1,12 +1,8 @@
 package jp.canetrash.kinesis.sample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		System.out.println("Heelo world");
 	}
 }
